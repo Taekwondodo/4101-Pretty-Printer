ad506c83a37c1e1ba74cd76c835586ab26c0e99d
Taekwondodo <mlawnc@gmail.com>
2016-09-16 23:21:53 -0500

    Fixed the issue with the "Illegal character ' '" thing. Turns out we also need to skip over carriage returns and new lines, ASCII 13 & 10 respectively

--- Parse/Scanner.cs
+++ Parse/Scanner.cs
@@ -32,17 +32,16 @@ namespace Parse
    
                 // TODO: skip white space and comments
                 
-                while(ch == ' ' || ch == ';')
+                while (ch == ' ' || ch == ';' || ch == 13 || ch == 10)
                 {
                     if (ch == ';')
                          In.ReadLine();
                     ch = In.Read();
                 }
-                    
-              
+
                 if (ch == -1)
                     return null;
-        
+
                 // Special characters
                 else if (ch == '\'')
                     return new Token(TokenType.QUOTE);
@@ -53,7 +52,7 @@ namespace Parse
                 else if (ch == '.')
                     // We ignore the special identifier `...'.
                     return new Token(TokenType.DOT);
-                
+
                 // Boolean constants
                 else if (ch == '#')
                 {
@@ -81,48 +80,49 @@ namespace Parse
                 {
                     int x;
                     ch = In.Read();
-                
+
                     for (x = 0; ch != '"'; x++)
                     {
-                        buf[x] = Convert.ToChar(ch);
+                        buf[x] = (char)(ch);
                         ch = In.Read();
                     }
                     // TODO: scan a string into the buffer variable buf
-                    return new StringToken(new String(buf, 0, x));
-                    
+                    return new StringToken(new String(buf, 0, x + 1));
+
                 }
 
-    
+
                 // Integer constants
                 else if (ch >= '0' && ch <= '9')
                 {
-                    int k, i;
+                    int i = 0;
                     // TODO: scan the number and convert it to an integer
-                    for(k = 0; ch != ' '; k++)
+                    do
                     {
-                        i = ch - '0';
-                        buf[k] = (char)i;
+                        i = i * 10 + (ch - '0'); // Adding another digit to the number increases the value of the digits by a factor of 10 each time
                         ch = In.Read();
-                    }
+                    } while (ch >= '0' && ch <= '9');
+
 
                     // make sure that the character following the integer
                     // is not removed from the input stream
                     return new IntToken(i);
                 }
-        
+
                 // Identifiers
                 else if (ch >= 'A' && ch <= 'Z'
                          // or ch is some other valid first character
                          // for an identifier
-                         ) {
+                         )
+                {
                     // TODO: scan an identifier into the buffer
 
                     // make sure that the character following the integer
                     // is not removed from the input stream
 
-                    return new IdentToken(new String(buf, 0, 0)); 
+                    return new IdentToken(new String(buf, 0, 0));
                 }
-    
+
                 // Illegal character
                 else
                 {
